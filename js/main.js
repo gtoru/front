d91e856a47247f393551e9566718b1bef35c098b
Roz1331 <45918469+Roz1331@users.noreply.github.com>
2020-03-23 12:44:20 +0300

    исправлена регистрация

--- js/main.js
+++ js/main.js
@@ -20,3 +20,45 @@ function show_hide_password(target) {
     }
     return false;
 }
+
+var sec = 00;
+var min = 30;
+function refresh() {
+    sec--;
+    if (sec == -01) {
+        sec = 59;
+        min = min - 1;
+    } else {
+        min = min;
+    }
+    if (sec <= 9) {
+        sec = "0" + sec;
+    }
+    time = (min <= 9 ? "0" + min : min) + ":" + sec;
+    if (document.getElementById) {
+        timer.innerHTML = time;
+    }
+    inter = setTimeout("refresh()", 1000);
+    if (min == '00' && sec == '00') {
+        sec = "00";
+        clearInterval(inter);
+        /* выводим сообщение в элемент с id="tut", например <div id="tut"></div> */
+        // tut.innerHTML = "Таймер завершил свою работу!";
+
+        /* либо модальное окно */
+        alert('Таймер завершил свою работу!');
+
+        /* либо переход на какой-то адрес */
+        //window.location='http://www.net-f.ru/';
+
+        /* либо любой другой Ваш код */
+    }
+}
+
+function check() {
+    var submit = document.getElementsByName('submit')[0];
+    if (document.getElementById('politics').checked)
+        submit.disabled = '';
+    else
+        submit.disabled = 'disabled';
+}
